e3baa472a9c0703560716285bf3b534ee8d60ab1
Macy Tam <macytt1@gmail.com>
2025-04-13 01:38:43 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,33 @@
+#https://github.com/macy-tam/lab10-MT-NB.git
+#Partner 1: Macy Tam
+#Partner 2: Nicholas Batchelor
 """
 calculator.py
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
 """
+import math
 # First example
 def add(a, b): 
-    pass
+    a+b
 
+def subtract(a, b):
+    a - b
+
+def multiply(a, b):
+    a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    b / a
+
+def logarithm(a, b):
+    if b<=0:
+        raise ValueError
+    math.log(b,a)
+
+def exponent(a, b):
+    a**b
 

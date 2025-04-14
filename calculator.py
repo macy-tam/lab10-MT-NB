e82a328bfc2ef10dcd0bd2465c96aab2f013abe1
Macy Tam <macytt1@gmail.com>
2025-04-13 22:24:11 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -18,29 +18,15 @@ def square_root(a):
 def hypotenuse(a, b):
     math.hypot(a, b)
 
-def add(a, b): 
-    a+b
-
 def subtract(a, b):
     a - b
-
-def multiply(a, b):
-    a * b
-
-
 
 def logarithm(a, b):
     if b<=0:
         raise ValueError
     math.log(b,a)
 
-def exponent(a, b):
-    a**b
-
-import math
 def add(a, b): a + b
-
-def sub(a, b): a - b
 
 def mul(a, b): a * b
 
@@ -48,10 +34,5 @@ def div(a, b): # raise ZeroDivisionError if a == 0
     if a == 0:
         raise ZeroDivisionError
     else: b / a
-
-def log(a, b): # use math library + raise ValueError
-    #did not say raise value error if
-    math.log(b, a)
-    raise ValueError
 
 def exp(a, b): a**b
